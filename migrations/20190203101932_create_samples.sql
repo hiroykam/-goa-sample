5214882422104e17eb03de28969d3d572a8262bd
hiroykam <hiroykam@gmail.com>
2019-02-03 18:40:17 +0900

    remove unncessary comment of migration file

--- migrations/20190203101932_create_samples.sql
+++ migrations/20190203101932_create_samples.sql
@@ -5,11 +5,11 @@ CREATE TABLE IF NOT EXISTS samples
 	user_id int unsigned NOT NULL,
 	name varchar(255) NOT NULL,
 	detail text NOT NULL,
-	created_at datetime DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '作成日',
-	updated_at datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL COMMENT '更新日',
-	deleted_at datetime COMMENT '削除日',
+	created_at datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	updated_at datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+	deleted_at datetime,
 	PRIMARY KEY (id)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT = 'inquiries';
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
 -- +migrate Down
 
